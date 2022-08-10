7aa2ca61e14b69376dd394e413d28f7400551046
RiveSunder <rivesunder@protonmail.com>
2022-08-10 16:02:12 -0600

    add dependencies to setup.py, numpy, jax, and coverage

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(\
         name="sepia", \
         packages=["sepia", "testing"], \
         version = "0.00000", \
+        install_requires =[\
+                "coverage==6.4.3",\
+                "jax==0.3.15",\
+                "jaxlib==0.3.15",\
+                "numpy==1.23.1",\
+                ] 
         description = "<<stealth-mode>>" \
     )
 
