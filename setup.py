9ad8e20de6e89cdebbaedfced615f2f9a7266751
RiveSunder <rivesunder@protonmail.com>
2022-09-09 12:29:29 -0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+#  packages=["sepia", "testing"], \
 
 setup(\
         name="sepia", \
-        packages=["sepia", "testing"], \
         version = "0.00000", \
         install_requires =[\
                 "coverage==6.4.3",\
                 "jax==0.3.15",\
                 "jaxlib==0.3.15",\
                 "numpy==1.23.1",\
-                ] 
-        description = "<<stealth-mode>>" \
+                ],\
+        packages=find_packages(),\
+        description = "Neuro/symbolic learning for biological sequences and strucutures"
     )
 
 
