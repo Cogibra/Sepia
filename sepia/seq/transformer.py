2e33238ea3db50300c63e91e73c8018b49f5f813
RiveSunder <rivesunder@protonmail.com>
2022-12-23 17:18:51 -0700

    signs of life: add optimizer_step function

--- sepia/seq/transformer.py
+++ sepia/seq/transformer.py
@@ -54,14 +54,16 @@ class Transformer():
         self.encoder_dim = self.token_dim * 3
 
         # expose these to user
-        self.encoder_size = 1
-        self.decoder_size = 1
+        self.seq_length = 32
+        self.encoder_size = 8
+        self.decoder_size = 8
         self.hidden_dim = 64
-        self.seq_length = 128
         self.mlp_hidden_dim = 48 
         self.mlp_activation = jax.nn.relu
         self.my_seed = 13
-        self.init_scale = 1e-3
+        self.init_scale = 1e-1
+        self.mask_rate = 0.2
+        self.lr = 1e-3
 
         self.initialize_model()
 
@@ -78,16 +80,16 @@ class Transformer():
         # encoder stack
         self.encoder_stack = []
         for ii in range(self.encoder_size): 
-            weights = npr.randn(self.token_dim, self.encoder_dim)
+            weights = npr.randn(self.token_dim, self.encoder_dim)*self.init_scale
             attention_weights = SelfAttentionW(weights = weights)
 
-            mlp_weights = [npr.randn(self.token_dim, self.mlp_hidden_dim), \
-                    npr.randn(self.mlp_hidden_dim, self.mlp_hidden_dim), \
-                    npr.randn(self.mlp_hidden_dim, self.token_dim)]
+            mlp_weights = [npr.randn(self.token_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.token_dim)*self.init_scale]
 
-            mlp_biases = [npr.randn(self.mlp_hidden_dim,), \
-                    npr.randn(self.mlp_hidden_dim,), \
-                    npr.randn(self.token_dim,)]
+            mlp_biases = [npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.token_dim,)*self.init_scale]
 
 
             mlp_params = MLPParams(mlp_weights=mlp_weights,\
@@ -103,23 +105,23 @@ class Transformer():
         # decoder stack
         self.decoder_stack = []
         for ii in range(self.decoder_size): 
-            mlp_weights = [npr.randn(self.token_dim, self.mlp_hidden_dim), \
-                    npr.randn(self.mlp_hidden_dim, self.mlp_hidden_dim), \
-                    npr.randn(self.mlp_hidden_dim, self.token_dim)]
+            mlp_weights = [npr.randn(self.token_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.token_dim)*self.init_scale]
 
-            mlp_biases = [npr.randn(self.mlp_hidden_dim,), \
-                    npr.randn(self.mlp_hidden_dim,), \
-                    npr.randn(self.token_dim,)]
+            mlp_biases = [npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.token_dim,)*self.init_scale]
 
             mlp_params = MLPParams(mlp_weights=mlp_weights,\
                     mlp_biases=mlp_biases)
 
             # decoder self-attention
-            weights_a = npr.randn(self.token_dim, self.encoder_dim)
+            weights_a = npr.randn(self.token_dim, self.encoder_dim)*self.init_scale
             self_attention_weights = SelfAttentionW(weights = weights_a)
 
             # encoder-decoder attention
-            weights_b = npr.randn(self.token_dim, self.encoder_dim)
+            weights_b = npr.randn(self.token_dim, self.encoder_dim)*self.init_scale
             encoded_weights = SelfAttentionW(weights = weights_b)
 
             attention_weights = EncodedAttentionW(\
@@ -206,31 +208,164 @@ class Transformer():
 
         input_tokens = bijective_forward(vector, self.token_parameters)[None,:,:]
 
-        parameters = (self.token_parameters, \
+        parameters = [self.token_parameters, \
                 self.encoder_stack, \
-                self.decoder_stack)
+                self.decoder_stack]
 
-        self.mask_rate = 0.2
 
         masked_tokens = input_tokens \
                 * (npr.rand(*input_tokens.shape[:2],1) > self.mask_rate)
 
         grad_loss = grad(self.get_loss, argnums=2)
-        loss = self.get_loss(masked_tokens, input_tokens, parameters)
 
+        # splitting these roles (returning loss or returning grads) might speed up training
+        loss = self.get_loss(masked_tokens, input_tokens, parameters)
         my_grad = grad_loss(masked_tokens, input_tokens, parameters)
 
-        import pdb; pdb.set_trace()
+        self.parameters = parameters
 
+        return loss, my_grad
 
-    def fit(self, **kwargs):
+    def optimizer_step(self, parameters, gradients):
+
+        """
+        (token_parameters, encoder_stack, decoder_stack)
+        
+        token_parameters: 
+            NICEParametersW = namedtuple("NICEParametersW", field_names=("weights"))
+        encoder_stack:
+            [EncoderParams = namedtuple("EncoderParams", field_names=("attention_weights", "mlp_params"))]
+            which includesi as attention_weights, mlp_params:
+                SelfAttentionW = namedtuple("SelfAttentionW", field_names=("weights"))
+                MLPParams = namedtuple("MLPParams", \
+                        field_names=("mlp_weights", "mlp_biases"))
+        decoder_stackk:
+            [DecoderParams = namedtuple("DecoderParams", field_names=("encoded_attention", "mlp_params"))]
+            which includes as encoded_attention, mlp_params:
+                EncodedAttentionW = namedtuple("EncodedAttentionW", \
+                        field_names=("self_weights", "encoded_weights")) 
+                which includes as self_weights, encoded_weights:
+                    SelfAttentionW = namedtuple("SelfAttentionW", field_names=("weights"))
+                    SelfAttentionW = namedtuple("SelfAttentionW", field_names=("weights"))
+                MLPParams = namedtuple("MLPParams", \
+                        field_names=("mlp_weights", "mlp_biases"))
+
+        """
+        
+        # SGD 
+        ## token params
+        token_weights = parameters[0].weights - self.lr * gradients[0].weights
+        self.token_parameters = NICEParametersW(weights=token_weights)
+
+        ## encoder_stack params
+        self.encoder_stack = []
+        for ii in range(len(parameters[1])):
+            self_weights = parameters[1][ii].attention_weights.weights \
+                    - self.lr * gradients[1][ii].attention_weights.weights
+            """
+            mlp_weights = [npr.randn(self.token_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.mlp_hidden_dim)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim, self.token_dim)*self.init_scale]
+
+            mlp_biases = [npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.mlp_hidden_dim,)*self.init_scale, \
+                    npr.randn(self.token_dim,)*self.init_scale]
+
+            mlp_params = MLPParams(mlp_weights=mlp_weights,\
+                    mlp_biases=mlp_biases)
+            """
+            mlp_weights = []
+            mlp_biases = []
+            for kk in range(len(parameters[1][ii].mlp_params.mlp_weights)):
+                mlp_weights.append(parameters[1][ii].mlp_params.mlp_weights[kk] - \
+                        self.lr * gradients[1][ii].mlp_params.mlp_weights[kk])
+                mlp_biases.append(parameters[1][ii].mlp_params.mlp_biases[kk] - \
+                        self.lr * gradients[1][ii].mlp_params.mlp_biases[kk])
+
+            mlp_params = MLPParams(mlp_weights=mlp_weights, mlp_biases=mlp_biases)
+
+            #mlp_params = parameters[1][ii].mlp_params
+
+            attention_weights = SelfAttentionW(weights=self_weights)
+            encoder_parameters = EncoderParams( \
+                    attention_weights = attention_weights, \
+                    mlp_params = mlp_params)
+
+            self.encoder_stack.append(encoder_parameters)
+
+        ## decoder stack params
+        self.decoder_stack = []
+        for jj in range(len(parameters[2])):
+
+            self_self_weights = parameters[2][jj].encoded_attention.self_weights.weights - \
+                    self.lr * gradients[2][jj].encoded_attention.self_weights.weights
+            encoded_self_weights = parameters[2][jj].encoded_attention.encoded_weights.weights - \
+                    self.lr * gradients[2][jj].encoded_attention.encoded_weights.weights
+
+            self_weights = SelfAttentionW(weights=self_self_weights)
+            encoded_weights = SelfAttentionW(weights=encoded_self_weights)
+
+            mlp_weights = []
+            mlp_biases = []
+            for kk in range(len(parameters[2][jj].mlp_params.mlp_weights)):
+                mlp_weights.append(parameters[2][jj].mlp_params.mlp_weights[kk] - \
+                        self.lr * gradients[2][jj].mlp_params.mlp_weights[kk])
+                mlp_biases.append(parameters[2][jj].mlp_params.mlp_biases[kk] - \
+                        self.lr * gradients[2][jj].mlp_params.mlp_biases[kk])
+
+            mlp_params = MLPParams(mlp_weights=mlp_weights, mlp_biases=mlp_biases)
+            #mlp_parms = parameters[2][jj].mlp_params
+
+            encoded_attention = EncodedAttentionW(self_weights=self_weights, \
+                    encoded_weights=encoded_weights)
+
+            decoder_parameters = DecoderParams( \
+                encoded_attention = encoded_attention, \
+                mlp_params = mlp_params)
+
+            self.decoder_stack.append(decoder_parameters)
+            #self.decoder_stack.append(DecoderParams(encoded_attention=encoded_attention, mlp_params=mlp_params))
+
+    def fit(self, dataloader, **kwargs) -> None:
         # training loop
 
-        max_steps = 10
+        # dataloader is an iterable that returns batches
+
+        max_steps = query_kwargs("max_steps", 100, **kwargs)
+        display_every = max_steps // 16
+        
         for step in range(max_steps):
-            pass
+            
+            cumulative_loss = None
+            for batch_index, batch in enumerate(dataloader):
+
+                loss, my_grad = self.train_step(batch)
+
+                cumulative_loss = loss if cumulative_loss is None else cumulative_loss+loss  
+
+                self.optimizer_step(self.parameters, my_grad)
+
+
+            if step % display_every == 0:
+                print(f"loss at step {step}:  {cumulative_loss / (batch_index+1.):.3e}")
+
+                
 
 
 if __name__ == "__main__":
 
-    pass
+    model = Transformer()
+
+    ha_tag = "YPYDVPDYA"
+
+    dataloader = [[ha_tag]]
+
+    print(model(ha_tag))
+
+    model.fit(dataloader, max_steps=5555)
+
+    print(model(ha_tag))
+
+    import pdb; pdb.set_trace()
+    #fit(self, dataloader, **kwargs) -> None:
+
