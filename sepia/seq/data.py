b7da5e14d8db1f643e8f446047f313d6f3a4f367
rivesunder <rivesunder@protonmail.com>
2023-01-18 12:34:48 -0700

    implement token dict and tokens to one-hot functionality in sepia.seq.data
    part of separating the sequence_to_vectors functionality into multiple steps to facilitate pre-processing and batching with vmap|pmap

--- sepia/seq/data.py
+++ sepia/seq/data.py
@@ -1,12 +1,13 @@
 from jax import numpy as jnp
 from jax import grad
+import jax
 
 import numpy as np
 import numpy.random as npr
 aa_keys = "arndcqeghilkmfpstwyvuox"
 NULL_ELEMENT = "-"
 
-def get_sequence_dict(vocabulary: str, vector_length: int=32, my_seed: int=42):
+def get_sequence_dict(vocabulary: str, vector_length: int=32, my_seed: int=42) -> dict:
 
     sequence_dict = {}
 
@@ -21,6 +22,35 @@ def get_sequence_dict(vocabulary: str, vector_length: int=32, my_seed: int=42):
 
 
     return sequence_dict
+
+def get_token_dict(vocabulary: str) -> dict:
+
+    token_dict = {}
+
+    vocabulary_list = list(vocabulary)
+    vocabulary_list.sort()
+
+    token_dict[NULL_ELEMENT] = np.array(0).reshape(-1,1)
+    for token, element in enumerate(vocabulary):
+        token_dict[element.lower()] = np.array(token + 1).reshape(1,-1)
+
+    return token_dict
+
+def token_to_one_hot(token: jnp.array, one_hot: jnp.array) -> jnp.array:
+
+    return one_hot.at[token].set(1.0)
+
+vmap_to_one_hots = jax.vmap(token_to_one_hot)
+
+def tokens_to_one_hot(tokens: jnp.array, max_length: int=None) -> jnp.array:
+
+    if max_length is None:
+        max_length = jnp.max(tokens)+1
+
+    # pre-allocate one-hot array
+    one_hots = jnp.zeros((tokens.shape[0], max_length))
+
+    return vmap_to_one_hots(tokens, one_hots)
 
 def sequence_to_vectors(sequence: str, sequence_dict: dict, pad_to: int=256) -> jnp.array:
 
